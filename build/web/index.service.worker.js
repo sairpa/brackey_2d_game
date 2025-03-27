0f6c07a142d33d89c5b8871848c2ce88908f8b25
Ajay <pranav.ajay2000@gmail.com>
2025-03-27 21:52:36 +0530

    Updated impl for gh pages

--- build/web/index.service.worker.js
+++ build/web/index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1743008356|5182684171';
+const CACHE_VERSION = '1743092290|47422258';
 /** @type {string} */
 const CACHE_PREFIX = 'shrula odyssey-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = 'shrula_odyssey.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["shrula_odyssey.html","shrula_odyssey.js","shrula_odyssey.offline.html","shrula_odyssey.icon.png","shrula_odyssey.apple-touch-icon.png","shrula_odyssey.audio.worklet.js","shrula_odyssey.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["shrula_odyssey.wasm","shrula_odyssey.pck"];
+const CACHEABLE_FILES = ["index.wasm","index.pck"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
