a90a9dcfb2915fd770b7b0b82e25f14bd0dde26f
Ajay <pranav.ajay2000@gmail.com>
2025-03-29 13:25:11 +0530

    Updated assets, level content and minor mechanic changes

--- Readme.md
+++ Readme.md
@@ -3,13 +3,19 @@
 
 #### My attempt at learning godot and following through the [Godot 2D platformer tutorial from Brackeys](https://www.youtube.com/watch?v=LOhfqjmasi0)
 
+All sourced assets:
+- [Brackeys' Platformer Bundle](https://brackeysgames.itch.io/brackeys-platformer-bundle)
+- [Girl Knight Character Asset](https://jumpbutton.itch.io/girlknightasset)
+- [joystick plugin for godot](https://godotengine.org/asset-library/asset/1787)
+
 ### unique additions so far:
 - Custom script additions:
     - A double jump mechanic for the 2d sprite
     - Updated behaviour for the enemy sprite
-    - Added joystick support for android [referenced asset from marcofzio](https://godotengine.org/asset-library/asset/1787)
+    - Added joystick support for android and web builds of mobile
 - Assets front:
     - A custom svg logo using figma
+    - Edited the colors of the 
     
 
 ### Upcoming todos:
--- scripts/shrula.gd
+++ scripts/shrula.gd
@@ -3,15 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 150
 const JUMP_VELOCITY = -400.0
-
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 var flag = false
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -19,12 +19,11 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_just_pressed("jump") and not flag:
 		velocity.y = JUMP_VELOCITY * 1.2
 		flag = true
-	
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	# Handle animations
 	if is_on_floor():
 		if direction == 0:
 			animated_sprite_2d.play("idle")
